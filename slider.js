0613cc82076887be908ab618f8b1ef5759b2f536
Oleksandr Butsyk <butsyk1@gmail.com>
2018-02-08 23:00:14 +0200

    first

---
+++ slider.js
@@ -0,0 +1,12 @@
+$(function(){
+$('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    randomStart: true,
+    speed: 1000,
+    touchEnabled: true,
+    auto: true,
+    pause: 4000,
+    slideWidth: 750
+  });
+});
